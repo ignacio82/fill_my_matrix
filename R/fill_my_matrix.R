6012ade17d8cdb90ff7b346f07283a8450248918
Ignacio Martinez <ignacio@protonmail.com>
2019-02-01 18:15:40 +0000

    bug fix

--- R/fill_my_matrix.R
+++ R/fill_my_matrix.R
@@ -1,9 +1,10 @@
 #'@export
 #'@useDynLib fortranMatrix, .registration = TRUE
 fill_my_matrix <- function(N=10, K=5, ncores=4) {
-  A <- .Fortran("fill", A = matrix(nrow = N, ncol = K),
+  A <- .Fortran("fill",
                 N = as.integer(N),
                 K = as.integer(K),
+                A = matrix(1982,nrow = N, ncol = K),
                 ncores = as.integer(ncores))
   return(A$A)
 }
