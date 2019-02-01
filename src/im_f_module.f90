81be399931d0a14aaf698dac422995c68cd4388d
Ignacio Martinez <ignacio@protonmail.com>
2019-02-01 18:15:59 +0000

    sets matrix elements to thread number

--- src/im_f_module.f90
+++ src/im_f_module.f90
@@ -7,14 +7,15 @@ subroutine fill_matrix(N,K,A,ncores) bind(C, name="fill_")
    use, intrinsic                                         :: iso_c_binding, only : c_double, c_int
    integer(c_int), intent(in)                             :: N,K, ncores
    real(c_double), DIMENSION(N, K), intent(out)	          :: A
-   integer                                                :: nn, kk
+   integer                                                :: nn, kk, thread_num
    ! Specify number of threads to use:
 	!$ call omp_set_num_threads(ncores)
+	!$omp parallel private(thread_num)
 	!$omp parallel do
 	do nn=1,N
 	   do kk=1,K
-	      CALL SLEEP(1)
-	      A(nn,kk) = nn + kk
+	      !$ thread_num = omp_get_thread_num()
+	      A(nn,kk) = thread_num
 	   end do
 	   !print *, A(nn, :)
 	end do
