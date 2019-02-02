c8c43f11308caf6ee396322f4d1e2d6443471e39
Ignacio Martinez <ignacio@protonmail.com>
2019-02-02 23:36:08 +0000

    fixes subroutine

--- src/im_f_module.f90
+++ src/im_f_module.f90
@@ -3,23 +3,22 @@ module im_f_module
    implicit none
    contains
 
-subroutine fill_matrix(N,K,A,ncores) bind(C, name="fill_")
+subroutine fill_matrix(N,K,A,nthreads) bind(C, name="fill_")
    use, intrinsic                                         :: iso_c_binding, only : c_double, c_int
-   integer(c_int), intent(in)                             :: N,K, ncores
-   real(c_double), DIMENSION(N, K), intent(out)	          :: A
+   integer(c_int), intent(in)                             :: N,K, nthreads
    integer                                                :: nn, kk, thread_num
-   ! Specify number of threads to use:
-	!$ call omp_set_num_threads(ncores)
-	!$omp parallel private(thread_num)
-	!$omp parallel do
-	do nn=1,N
-	   do kk=1,K
-	      !$ thread_num = omp_get_thread_num()
-	      A(nn,kk) = thread_num
-	   end do
-	   !print *, A(nn, :)
-	end do
-	!$omp end parallel do
+   real(c_double), DIMENSION(N, K), intent(out)	          :: A
+    ! Specify number of threads to use:
+  	!$ call omp_set_num_threads(nthreads)
+  	!$omp parallel do private(thread_num)
+  	do nn=1,N
+  	   do kk=1,K
+  	      !$ thread_num = omp_get_thread_num()
+  	      A(nn,kk) = thread_num
+  	   end do
+  	   !print *, A(nn, :)
+  	end do
+  	!$omp end parallel do
 end subroutine fill_matrix
 
 
