0f2cc149baaea50d7de84ba7c755c59ffe890ada
Ignacio Martinez <ignacio@protonmail.com>
2019-02-02 23:35:43 +0000

    set number of threads with OpenMPController

--- R/fill_my_matrix.R
+++ R/fill_my_matrix.R
@@ -1,10 +1,11 @@
 #'@export
 #'@useDynLib fortranMatrix, .registration = TRUE
-fill_my_matrix <- function(N=10, K=5, ncores=4) {
+fill_my_matrix <- function(N=10, K=5, nthreads=4) {
+  (OpenMPController::omp_set_num_threads(nthreads))
   A <- .Fortran("fill",
                 N = as.integer(N),
                 K = as.integer(K),
                 A = matrix(1982,nrow = N, ncol = K),
-                ncores = as.integer(ncores))
+                nthreads = as.integer(nthreads))
   return(A$A)
 }
