f5dfb433602e842f4d61fd2ccdcea04d85fe341e
Ignacio Martinez email = IMartinez@mathematica-mpr.com <IMartinez@mathematica-mpr.com>
2019-02-04 08:32:22 -0500

    more complicated math

--- src/im_f_module.f90
+++ src/im_f_module.f90
@@ -10,16 +10,22 @@ subroutine fill_matrix(N,K,A,nthreads) bind(C, name="fill_")
    real(c_double), DIMENSION(N, K), intent(out)	          :: A
     ! Specify number of threads to use:
   	!$ call omp_set_num_threads(nthreads)
-  	!$omp parallel do private(thread_num)
+  	!$omp parallel do 
   	do nn=1,N
   	   do kk=1,K
-  	      !$ thread_num = omp_get_thread_num()
-  	      A(nn,kk) = thread_num
+  	      CALL do_something(i=DBLE(nn),k=DBLE(kk), ik= A(nn,kk))
   	   end do
   	   !print *, A(nn, :)
   	end do
   	!$omp end parallel do
 end subroutine fill_matrix
+
+ 
+ subroutine do_something(i,k, ik)
+    double precision, intent(in)                       :: i, k 
+    double precision, intent(out)                      :: ik 
+      ik = 1 / (1 + exp(i**2 + i**3 + k**2 + k**3))
+ end subroutine do_something
 
 
 
